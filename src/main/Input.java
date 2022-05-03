251c860996325dc8503327b47007dbd6d4b6f849
funak <funak.attila@gmail.com>
2022-05-03 19:50:00 +0200

    Add newInput method to Input

--- src/main/Input.java
+++ src/main/Input.java
@@ -1,4 +1,11 @@
 package main;
 
+import java.util.Scanner;
+
 public class Input {
+    public String newInput(String message) {
+        System.out.println(message);
+        Scanner userInput = new Scanner(System.in);
+        return userInput.nextLine();
+    }
 }
